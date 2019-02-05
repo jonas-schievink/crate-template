d1cc3630b0605b86a0d5a2d47f68f76b01cc2c3f
Jonas Schievink <jonasschievink@gmail.com>
2019-02-05 14:57:31 +0100

    start out with crate-level docs
    they're still a TODO, but at least don't trigger the lint

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,5 @@
-// TODO: Write crate docs
+//! TODO: Write crate docs
 
 #![doc(html_root_url = "https://docs.rs/{{CRATE}}/0.0.0")]
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
-
-// TODO: Write the actual crate
