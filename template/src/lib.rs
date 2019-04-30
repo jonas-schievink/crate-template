d443be6d54196ed25abcf1451f2553016bb4f48b
Jonas Schievink <jonasschievink@gmail.com>
2019-04-30 22:17:54 +0200

    Enable Rust 2018 idiom lints
    Closes #7

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,4 +1,7 @@
 //! TODO: Write crate docs
 
 #![doc(html_root_url = "https://docs.rs/{{CRATE}}/0.0.0")]
-#![warn(missing_debug_implementations)]
+#![warn(
+    missing_debug_implementations,
+    rust_2018_idioms
+)]
