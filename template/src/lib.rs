90dd4053341aebd7ecb25446c956b77ad9ca1c39
Jonas Schievink <jonasschievink@gmail.com>
2019-05-01 00:02:05 +0200

    Reformat warn attribute

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,4 @@
 //! TODO: Write crate docs
 
 #![doc(html_root_url = "https://docs.rs/{{CRATE}}/0.0.0")]
-#![warn(
-    missing_debug_implementations,
-    rust_2018_idioms
-)]
+#![warn(missing_debug_implementations, rust_2018_idioms)]
