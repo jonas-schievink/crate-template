48cc1f84cadf8a44b1146a7ed4510247941719f3
Jonas Schievink <jonasschievink@gmail.com>
2019-05-29 17:38:17 +0200

    Deny a few warnings in doctests

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,4 +1,8 @@
 //! TODO: Write crate docs
 
 #![doc(html_root_url = "https://docs.rs/{{CRATE}}/0.0.0")]
+
+// Deny a few warnings in doctests, since rustdoc `allow`s many warnings by default
+#![doc(test(attr(deny(unused_imports, unused_must_use))))]
+
 #![warn(missing_debug_implementations, rust_2018_idioms)]
